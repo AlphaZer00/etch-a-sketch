86c0e90932eb24c51c4f6fae6f0ce8e293c7814f
AlphaZer00 <batudasdemir@gmail.com>
2023-09-22 12:07:08 -0400

    Add dynamic, slider-controlled grid size

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 
-const gridContainer = document.querySelector(".grid-container");
 
-for (let i=0; i < 256; i++) {
-    const box = document.createElement("div");
-    gridContainer.appendChild(box);
-    box.classList.add("boxes");
+// Update Range Slider Input
+const sliderInput = document.querySelector("input");
+const number = document.querySelector(".slider-num");
+sliderInput.addEventListener("input", () => {
+    size = sliderInput.value;
+    updateGridSize(size);
+    number.textContent= size;
+});
+
+
+
+function updateGridSize(size) {
+
+    const gridContainer = document.querySelector(".grid-container");
+    gridContainer.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    const squares = gridContainer.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+
+    amount = sliderInput.value * sliderInput.value;
+
+    for (let i=0; i < amount; i++) {
+        const box = document.createElement("div");
+        gridContainer.appendChild(box);
+        box.classList.add("boxes");
+    }
 }
-
-
 
